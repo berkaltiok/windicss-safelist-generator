0573fca5f5484301cc1393003cbc6d20a98f3ba0
Berk Altıok <berkpw@gmail.com>
2021-11-21 20:12:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const plugin = require('tailwindcss/plugin')
+const plugin = require('windicss/plugin')
 const generator = require('./src/generator')
 
 module.exports = plugin.withOptions(({ path = 'safelist.txt', patterns = [] }) => ({ theme }) =>
