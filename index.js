44b860612b83ef3e4ce3bb333dd386ba4024828b
Berk Altıok <berkpw@gmail.com>
2021-11-26 00:35:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const plugin = require('windicss/plugin')
+const plugin = require('windicss/plugin');
 const generator = require('./src/generator')
 
 module.exports = plugin.withOptions(({ path = 'safelist.txt', patterns = [] }) => ({ theme }) =>
-    fs.writeFileSync(path, generator(theme)(patterns).join('\n')))
+    fs.writeFileSync(path, JSON.stringify(generator(theme)(patterns))))
